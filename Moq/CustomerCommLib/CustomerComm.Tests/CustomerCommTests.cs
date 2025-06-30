0f511bf3ec7b3b260d32b521d7b139c7c330dc03
Krishna-2005 <senthilsenthil80560@gmail.com>
2025-06-30 16:01:46 +0530

    Add myfolder to the repository

---
+++ Moq/CustomerCommLib/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mockMailSender;
+        private CustomerCommLib.CustomerComm _customerComm;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+            _mockMailSender
+                .Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            _customerComm = new CustomerCommLib.CustomerComm(_mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            var result = _customerComm.SendMailToCustomer();
+            Assert.IsTrue(result);
+        }
+    }
+}
+
