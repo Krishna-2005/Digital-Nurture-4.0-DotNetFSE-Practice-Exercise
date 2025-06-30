307591a8ec4381e5567a99d1eedefaa5b446818b
Krishna-2005 <senthilsenthil80560@gmail.com>
2025-06-30 19:30:20 +0530

    Push Week2-Mandatory-Hands-On to main branch

---
+++ NUnit/CalcLibrary/CalcLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,60 @@
+﻿using CalcLibrary;
+using NUnit.Framework;
+using System;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        [TestCase(2, 3, 5)]
+        [TestCase(0, 0, 0)]
+        public void Add_ShouldReturnCorrectSum(int a, int b, int expected)
+        {
+            var result = _calculator.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(5, 3, 2)]
+        [TestCase(0, 0, 0)]
+        public void Subtract_ShouldReturnCorrectDifference(int a, int b, int expected)
+        {
+            var result = _calculator.Subtract(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(2, 3, 6)]
+        [TestCase(0, 10, 0)]
+        public void Multiply_ShouldReturnCorrectProduct(int a, int b, int expected)
+        {
+            var result = _calculator.Multiply(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(10, 2, 5)]
+        [TestCase(9, 3, 3)]
+        public void Divide_ShouldReturnCorrectQuotient(int a, int b, double expected)
+        {
+            var result = _calculator.Divide(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Divide_ByZero_ShouldThrowException()
+        {
+            Assert.Throws<DivideByZeroException>(() => _calculator.Divide(10, 0));
+        }
+    }
+}
